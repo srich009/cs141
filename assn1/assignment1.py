b30e3ac693ed46931855900e936d7cbd6fc0adc7
srich009 <srich009@ucr.edu>
2017-04-23 06:33:18 +0000

    hw1 divide && conquer part

--- assn1/assignment1.py
+++ assn1/assignment1.py
@@ -1,16 +1,17 @@
 import sys
 from math import sqrt
 import re
+from operator import itemgetter
 
-pointRE=re.compile("(-?\\d+.?\\d*)\\s(-?\\d+.?\\d*)")
+pointRE = re.compile("(-?\\d+.?\\d*)\\s(-?\\d+.?\\d*)")
 
 def dist(p1, p2):
-    return sqrt(pow(float(p1[0])-float(p2[0]),2) + pow(float(p1[1])-float(p2[1]),2)) # modified so casts to float
+    return sqrt(pow((p1[0]-p2[0]),2) + pow((p1[1]-p2[1]),2))
 #----------------------------------------------------------------------
 
 #Brute force version of the nearest neighbor algorithm, O(n**2)
 def brute_force_nearest_neighbor(points):
-    min_distance=0
+    min_distance = 0
     if len(points) < 2:
         return min_distance
 
@@ -32,12 +33,65 @@ def brute_force_nearest_neighbor(points):
 
 #Run the divide-and-conquor nearest neighbor 
 def nearest_neighbor(points):
-    return nearest_neighbor_recursion(points)
+    x_pnt = sorted(points, key=itemgetter(0)) # points sorted by x
+    y_pnt = sorted(points, key=itemgetter(1)) # points sorted by y
+    # print x_pnt
+    # print y_pnt
+    return nearest_neighbor_recursion(x_pnt, y_pnt)
 #----------------------------------------------------------------------
 
 #Divide and conquer recurse part
-def nearest_neighbor_recursion(points):
-    min_distance=0
+def nearest_neighbor_recursion(x_pnt, y_pnt):
+    min_distance = 0
+    num_p = len(x_pnt)
+
+    if num_p <= 3:
+    	return brute_force_nearest_neighbor(x_pnt)
+    else:
+    	half = num_p/2			# half way
+    	xpl = x_pnt[:half]		# x points left half
+    	xpr = x_pnt[half:]		# x points right half
+
+        med = xpl[-1]           # median point
+        ypl, ypr = [], []       # y left half, y right half
+
+        # sort points by y
+        for pnt in y_pnt:
+            if pnt[0] <= med[0]: 
+                ypl.append(pnt) # add pnt y left half
+            else:
+                ypr.append(pnt) # add pnt y right half
+
+        # print xpl
+        # print xpr
+        # print ypl
+        # print ypr
+        # print disr_l
+        # print dist_r
+
+        dist_l = nearest_neighbor_recursion(xpl, ypl)
+        dist_r = nearest_neighbor_recursion(xpr, ypr)
+
+        if dist_l < dist_r:
+            dist_m = dist_l
+        else: 
+            dist_m = dist_r
+
+        stp = [] # strip of points in the middle
+
+        for pnt in y_pnt:
+            if abs(pnt[0] - med[0]) < dist_m:
+                stp.append(pnt)
+
+        num_stp = len(stp)
+        min_distance = dist_m
+
+        if num_stp > 1:
+            for i in range(num_stp-1):
+                for j in range(i+1, min(i+8, num_stp)):
+                    if dist(stp[i],stp[j]) < min_distance:
+                        min_distance = dist(stp[i],stp[j])
+
     return min_distance
 #----------------------------------------------------------------------
 
@@ -52,8 +106,8 @@ def read_file(filename):
         line = line.strip()
         point_match=pointRE.match(line)
         if point_match:
-            x = point_match.group(1)
-            y = point_match.group(2)
+            x = float(point_match.group(1)) # cast float
+            y = float(point_match.group(2)) # cast float
             points.append((x,y))
     print(points)
     return points
@@ -62,8 +116,8 @@ def read_file(filename):
 def main(filename,algorithm):
     algorithm=algorithm[1:]
     print algorithm
-    points=read_file(filename)
-    if algorithm =='dc':
+    points = read_file(filename)
+    if algorithm == 'dc':
         print("Divide and Conquer: ", nearest_neighbor(points))
     if algorithm == 'bf':
         print("Brute Force: ", brute_force_nearest_neighbor(points))
