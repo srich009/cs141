7cf232b03a0a5b1a8d396bf726e35c15c172b7aa
srich009 <srich009@ucr.edu>
2017-05-01 19:41:11 +0000

    final layout

--- assn1/extras/nearest_neighbor_v2.py
+++ assn1/extras/nearest_neighbor_v2.py
@@ -1,15 +1,19 @@
-import sys
-from math import sqrt
+# use python || python3
+
 import re
+import sys
+import time
+from math import sqrt
 from operator import itemgetter
 
 pointRE = re.compile("(-?\\d+.?\\d*)\\s(-?\\d+.?\\d*)")
 
 def dist(p1, p2):
     return sqrt(pow((p1[0]-p2[0]),2) + pow((p1[1]-p2[1]),2))
+
 #----------------------------------------------------------------------
 
-#Brute force version of the nearest neighbor algorithm, O(n**2)
+# Brute force version of the nearest neighbor algorithm, O(n**2)
 def brute_force_nearest_neighbor(points):
     min_distance = 0
     if len(points) < 2:
@@ -22,60 +26,49 @@ def brute_force_nearest_neighbor(points):
     # point p, where p[0] = x, p[1] = y
     # i = p1, j = p2
     for i in range(0, len(points)-1):
-        cur_i = points[i]
         for j in range(i+1, len(points)):
-            cur_j = points[j] 
-            cur_dist = dist(cur_i, cur_j)
+            cur_dist = dist(points[i], points[j])
             if cur_dist < min_distance:
                 min_distance = cur_dist
     return min_distance
 #----------------------------------------------------------------------
 
-#Run the divide-and-conquor nearest neighbor 
+# Run the divide-and-conquor nearest neighbor 
 def nearest_neighbor(points):
-    x_pnt = sorted(points, key=itemgetter(0)) # points sorted by x
-    y_pnt = sorted(points, key=itemgetter(1)) # points sorted by y
-    return nearest_neighbor_recursion(x_pnt, y_pnt)
+    return nearest_neighbor_recursion(points)
 #----------------------------------------------------------------------
 
-#Divide and conquer recurse part
-def nearest_neighbor_recursion(x_pnt, y_pnt):
+# Divide and conquer recurse part
+def nearest_neighbor_recursion(points):
+
     min_distance = 0
-    num_p = len(x_pnt)
+    num_p = len(points)
+    x_pnt = sorted(points, key=itemgetter(0)) # points sorted by x
+    y_pnt = sorted(points, key=itemgetter(1)) # points sorted by y
 
     if num_p <= 3:
         return brute_force_nearest_neighbor(x_pnt)
     else:
-        half = num_p/2          # half way
+        half = int(num_p/2)     # half way
         xpl = x_pnt[:half]      # x points left half
         xpr = x_pnt[half:]      # x points right half
+
         med = xpl[-1]           # median point
-        ypl, ypr = [], []       # y left half, y right half
 
-        # sort points by y
+        dist_l = nearest_neighbor_recursion(xpl)    # distance right half
+        dist_r = nearest_neighbor_recursion(xpr)    # distance left half
+        min_distance = min(dist_l, dist_r)
+
+        stp = [] # strip for points in the middle
+
+        # fill strip with points
         for pnt in y_pnt:
-            if pnt[0] <= med[0]: 
-                ypl.append(pnt) # add pnt y left half
-            else:
-                ypr.append(pnt) # add pnt y right half
-
-        dist_l = nearest_neighbor_recursion(xpl, ypl)
-        dist_r = nearest_neighbor_recursion(xpr, ypr)
-
-        if dist_l < dist_r:
-            dist_m = dist_l
-        else: 
-            dist_m = dist_r
-
-        stp = [] # strip of points in the middle
-
-        for pnt in y_pnt:
-            if abs(pnt[0] - med[0]) < dist_m:
+            if abs(pnt[0] - med[0]) < min_distance:
                 stp.append(pnt)
 
         num_stp = len(stp)
-        min_distance = dist_m
 
+        # look in the strip for a smaller distance
         if num_stp > 1:
             for i in range(num_stp-1):
                 for j in range(i+1, min(i+8, num_stp)):
@@ -86,41 +79,66 @@ def nearest_neighbor_recursion(x_pnt, y_pnt):
 #----------------------------------------------------------------------
 
 def read_file(filename):
-    points=[]
+    points = []
     # File format
     # x1 y1
     # x2 y2
     # ...
-    in_file=open(filename,'r')
+    in_file = open(filename,'r')
     for line in in_file.readlines():
         line = line.strip()
-        point_match=pointRE.match(line)
+        point_match = pointRE.match(line)
         if point_match:
             x = float(point_match.group(1)) # cast float
             y = float(point_match.group(2)) # cast float
             points.append((x,y))
-    print(points)
+    # print(points)
     return points
 #----------------------------------------------------------------------
 
 def main(filename,algorithm):
-    algorithm=algorithm[1:]
+    algorithm = algorithm[1:]
     print(algorithm)
     points = read_file(filename)
+    out_file = open( (filename[:-4] + "_distance.txt"), "a+")
+
     if algorithm == 'dc':
-        print("Divide and Conquer: ", nearest_neighbor(points))
+        dc = nearest_neighbor(points)
+        out_file.write(str(dc))
+        print("Divide and Conquer: ", dc)
+
     if algorithm == 'bf':
-        print("Brute Force: ", brute_force_nearest_neighbor(points))
+        bf = brute_force_nearest_neighbor(points)
+        out_file.write(str(bf))
+        print("Brute Force: ", bf)
+
     if algorithm == 'both':
-        print("Divide and Conquer: ", nearest_neighbor(points))
-        print("Brute Force: ", brute_force_nearest_neighbor(points))
+        deec = nearest_neighbor(points)
+        beef = brute_force_nearest_neighbor(points)
+        out_file.write(str(deec) + "\n" + str(beef) )
+        print("Divide and Conquer: ", deec)
+        print("Brute Force: ", beef)
+
+    if algorithm == 'time': # extra timing testing flag
+        out_file.write(filename + "\n")
+        start = time.clock()
+        out_file.write("Divide and Conquer: " + str(nearest_neighbor(points)) + "\n")
+        fin = time.clock()
+        out_file.write("Time: " + str(fin-start) + " sec"+ "\n")
+        out_file.write("\n")
+        start = time.clock()
+        out_file.write("Brute Force: " + str(brute_force_nearest_neighbor(points)) + "\n")
+        fin = time.clock()
+        out_file.write("Time: " + str(fin-start) + " sec" + "\n")
+        
+    out_file.close()
 #----------------------------------------------------------------------
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print("python assignment1.py -<dc|bf|both> <input_file>")
+        print("python nearest_neighbor.py -<dc|bf|both> <input_file>")
         quit(1)
     if len(sys.argv[1]) < 2:
-        print("python assignment1.py -<dc|bf|both> <input_file>")
+        print("python nearest_neighbor.py -<dc|bf|both> <input_file>")
         quit(1)
     main(sys.argv[2],sys.argv[1])
--- assn1/extras/test.sh
+++ assn1/extras/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# testing the algorithms for each input file
 python nearest_neighbor.py -time input_10.txt  
 python nearest_neighbor.py -time input_100.txt 
 python nearest_neighbor.py -time input_10e5.txt
