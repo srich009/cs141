edcfd28768d8d11e63a171339464a29edc91391b
srich009 <srich009@ucr.edu>
2017-06-06 03:29:59 +0000

    algorithms up

--- assn2/assignment2.py
+++ assn2/assignment2.py
@@ -1,24 +1,91 @@
+#---------------------------
+# weighted graph algorithms
+# all points shortest path
+#---------------------------
+
 import sys
 import re
 import time
 
-graphRE=re.compile("(\\d+)\\s(\\d+)")
-edgeRE=re.compile("(\\d+)\\s(\\d+)\\s(-?\\d+)")
+graphRE = re.compile("(\\d+)\\s(\\d+)")
+edgeRE = re.compile("(\\d+)\\s(\\d+)\\s(-?\\d+)")
 
-vertices=[]
-edges=[]
+vertices = []
+edges = []
+#------------------------------------------------------------------------------------------------
 
+# all points
 def BellmanFord(G):
-    pathPairs=[]
-    # Fill in your Bellman-Ford algorithm here
+    pathPairs = []
     # The pathPairs list will contain the list of vertex pairs and their weights [((s,t),w),...]
+    # Fill in your Bellman-Ford algorithm here
+    
+    temp = []  
+    n = len(G[0]) # number of verticies
+   
+    for i in range(n):
+        for j in range(n):
+            temp = BelmanFord_SS(G,i)
+            pathPairs.append( ( (i,j) , int(temp[j]) ) )
+
+    print (pathPairs)
     return pathPairs
+#-------------------------------------------------------------------------------------------------
+
+# single source
+def BelmanFord_SS(G,s):
+
+    d = []
+    n = len(G[0]) # number of verticies
+
+    # Step 1: initialize graph
+    for i in range(n):
+        d.append(float('inf'))
+   
+    d[s] = 0  
+
+    # Step 2: relax edges repeatedly
+    for k in range(n):
+        for u in range(n):
+            for v in range(n):
+                if (float(d[u]) + float(edges[u][v])) < float(d[v]):
+                    d[v] = float(d[u]) + float(edges[u][v])
+
+   # Step 3: check for negative-weight cycles
+    for u in range(n):
+        for v in range(n):
+            if float(d[u]) + float(edges[u][v]) < float(d[v]):
+                return false
+
+    return d
+#-------------------------------------------------------------------------------------------------
 
 def FloydWarshall(G):
-    pathPairs=[]
-    # Fill in your Floyd-Warshall algorithm here
+    pathPairs = []
     # The pathPairs list will contain the list of vertex pairs and their weights [((s,t),w),...]
+    # Fill in your Floyd-Warshall algorithm here
+
+    n = len(G[0]) # number of verticies
+    
+    # set vertex to self = 0
+    for i in range(n):
+        edges[i][i] = 0
+
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if float(edges[i][k]) == float('inf') or float(edges[k][j]) == float('inf'):
+                    continue
+                if float(edges[i][j]) > float(edges[i][k]) + float(edges[k][j]):
+                    edges[i][j] = int(float(edges[i][k]) + float(edges[k][j]))
+
+    for i in range(n):
+        for j in range(n):
+            pathPairs.append( ( (i,j) , edges[i][j] ) )
+
+    print (pathPairs)
     return pathPairs
+#-------------------------------------------------------------------------------------------------
 
 def readFile(filename):
     global vertices
@@ -27,38 +94,39 @@ def readFile(filename):
     # <# vertices> <# edges>
     # <s> <t> <weight>
     # ...
-    inFile=open(filename,'r')
-    line1=inFile.readline()
-    graphMatch=graphRE.match(line1)
+    inFile = open(filename,'r')
+    line1 = inFile.readline()
+    graphMatch = graphRE.match(line1)
     if not graphMatch:
-        print(line1+" not properly formatted")
+        print(line1 + " not properly formatted")
         quit(1)
-    vertices=list(range(int(graphMatch.group(1))))
-    edges=[]
+    vertices = list(range(int(graphMatch.group(1))))
+    edges = []
     for i in range(len(vertices)):
-        row=[]
+        row = []
         for j in range(len(vertices)):
             row.append(float("inf"))
         edges.append(row)
     for line in inFile.readlines():
         line = line.strip()
-        edgeMatch=edgeRE.match(line)
+        edgeMatch = edgeRE.match(line)
         if edgeMatch:
-            source=edgeMatch.group(1)
-            sink=edgeMatch.group(2)
+            source = edgeMatch.group(1)
+            sink = edgeMatch.group(2)
             if int(source) > len(vertices) or int(sink) > len(vertices):
-                print("Attempting to insert an edge between "+source+" and "+sink+" in a graph with "+vertices+" vertices")
+                print("Attempting to insert an edge between " + source + " and " + sink + " in a graph with " + vertices + " vertices")
                 quit(1)
-            weight=edgeMatch.group(3)
-            edges[int(source)-1][int(sink)-1]=weight
+            weight = edgeMatch.group(3)
+            edges[int(source)][int(sink)] = weight
     #Debugging
     #for i in G:
         #print(i)
     return (vertices,edges)
+#-------------------------------------------------------------------------------------------------
 
 def main(filename,algorithm):
-    algorithm=algorithm[1:]
-    G=readFile(filename)
+    algorithm = algorithm[1:]
+    G = readFile(filename)
     # G is a tuple containing a list of the vertices, and a list of the edges
     # in the format ((source,sink),weight)
     if algorithm == 'b' or algorithm == 'B':
@@ -66,19 +134,22 @@ def main(filename,algorithm):
     if algorithm == 'f' or algorithm == 'F':
         FloydWarshall(G)
     if algorithm == "both":
-        start=time.clock()
+        start = time.clock()
         BellmanFord(G)
-        end=time.clock()
-        BFTime=end-start
+        end = time.clock()
+        BFTime = end - start
+        start = time.clock()
         FloydWarshall(G)
-        start=time.clock()
-        end=time.clock()
-        FWTime=end-start
-        print("Bellman-Ford timing: "+str(BFTime))
-        print("Floyd-Warshall timing: "+str(FWTime))
+        end = time.clock()
+        FWTime = end - start
+        print("Bellman-Ford timing: " + str(BFTime))
+        print("Floyd-Warshall timing: " + str(FWTime))
+#-------------------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print("python bellman_ford.py -<f|b> <input_file>")
+        print("python bellman_ford.py -<f|b> <input_file>") # remember to rename the file later
         quit(1)
     main(sys.argv[2],sys.argv[1])
+#-------------------------------------------------------------------------------------------------
+
